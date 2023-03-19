05022360e23f2d1b8a447c5dff1eb1b36d8f1c6f
yas-say <yasowant@live.com>
2023-03-18 23:13:54 -0400

    updated read function

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-open("myfile.txt")
+# file = open("myfile.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+with open("myfile.txt") as file:
+    contents = file.read()
+    print(contents)
